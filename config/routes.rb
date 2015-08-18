e49165035d84a30f4ec60cb946e7265fe08e05a0
dajvido <dawid.jaskot@gmail.com>
2015-08-19 00:36:43 +0200

    Fix route for reports subjects

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
 
   resources :teachers
 
-  get 'reports/subjects', to: 'reports#subjects'
+  get 'reports/subjects', to: 'reports#subjects', as: 'report_subjects'
 end
