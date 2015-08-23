15efb05c91432fae96d528878beec649e8d1a5c0
dajvido <dawid.jaskot@gmail.com>
2015-08-23 02:12:18 +0200

    Add tests for payment decorator

--- spec/decorators/payment_decorator_spec.rb
+++ spec/decorators/payment_decorator_spec.rb
@@ -1,4 +1,17 @@
 require 'spec_helper'
 
 describe PaymentDecorator do
+  let(:payment) { create :payment, amount: 700 }
+
+  describe "#payment_date" do
+    subject { payment.decorate.payment_date }
+    it { is_expected.to eq payment.created_at.strftime("%d %b %Y") }
+  end
+
+  describe "#dollars" do
+    subject { payment.decorate.dollars }
+    it { is_expected.to eq '$700.00' }
+  end
+
+
 end
